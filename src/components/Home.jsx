a39fde2d46c07f2abdc1af27c3a23f641b3ab6ba
Arshiya Nandy <arshiyanandy1731@gmail.com>
2025-05-30 01:34:57 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,16 @@ const Home = () => {
         <div className='flex flex-col items-center justify-center h-full max-w-screen-lg px-4 mx-auto md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl font-bold text-white sm:text-7xl'>
-                    I'm a Front End Developer
+                    I'm a Front End Developer, ML Enthusiast
                 </h2>
                 <p className='max-w-md py-4 text-gray-500'>
-                    I have 2 years of experience in building and developing 
-                    websites.
-                    I am familiar with web-application technologies like
-                    React ,TailwindCSS, Next JS and JavaScript.
+                With over two years of experience in crafting modern, responsive web applications,
+                I specialize in technologies like React, Tailwind CSS, JavaScript, Typescript and Next.js.
+                    My development journey also expands into the realm of Machine Learning,
+                    where I’ve built AI based systems using tools such as Python, Pandas, NumPy, scikit-learn, and Hugging Face.
+                            I love to turn ideas into impactful, user-focused digital solutions.
+
+
                 </p>
                 <div>
                     <Link to ="portfolio" smooth duration={500} className='flex items-center px-4 py-3 my-2 text-white rounded-md cursor-pointer group w-fit bg-gradient-to-r from-cyan-500 to-blue-500'>
